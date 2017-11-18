0676d270120f4b08f24f2828f8955999ed1674ed
Alexandre <alexandreh133@gmail.com>
2017-11-17 22:25:46 -0200

    initial commit

---
+++ recomendacao.py
@@ -0,0 +1,78 @@
+##Base da dados
+from math import sqrt
+
+avaliacoes = {'Ana': 
+		{'Freddy x Jason': 2.5, 
+		 'O Ultimato Bourne': 3.5,
+		 'Star Trek': 3.0, 
+		 'Exterminador do Futuro': 3.5, 
+		 'Norbit': 2.5, 
+		 'Star Wars': 3.0},
+	 
+	  'Marcos': 
+		{'Freddy x Jason': 3.0, 
+		 'O Ultimato Bourne': 3.5, 
+		 'Star Trek': 1.5, 
+		 'Exterminador do Futuro': 5.0, 
+		 'Star Wars': 3.0, 
+		 'Norbit': 3.5}, 
+
+	  'Pedro': 
+                {'Freddy x Jason': 2.5, 
+		 'O Ultimato Bourne': 3.0,
+		 'Exterminador do Futuro': 3.5, 
+		 'Star Wars': 4.0},
+			 
+	  'Claudia': 
+		{'O Ultimato Bourne': 3.5, 
+		 'Star Trek': 3.0,
+		 'Star Wars': 4.5, 
+		 'Exterminador do Futuro': 4.0, 
+		 'Norbit': 2.5},
+				 
+	  'Adriano': 
+		{'Freddy x Jason': 3.0, 
+		 'O Ultimato Bourne': 4.0, 
+		 'Star Trek': 2.0, 
+		 'Exterminador do Futuro': 3.0, 
+		 'Star Wars': 3.0,
+		 'Norbit': 2.0}, 
+
+	  'Janaina': 
+	     {'Freddy x Jason': 3.0, 
+	      'O Ultimato Bourne': 4.0,
+	      'Star Wars': 3.0, 
+	      'Exterminador do Futuro': 5.0, 
+	      'Norbit': 3.5},
+			  
+	  'Leonardo': 
+	    {'O Ultimato Bourne':4.5,
+             'Norbit':1.0,
+	     'Exterminador do Futuro':4.0}
+}
+
+##Função da distância euclidiana
+def distancia_euclidiana(user1, user2):
+    si = {}
+    for item in avaliacoes[user1]: ##Retorna todos os itens que o user assistiu
+        if item in avaliacoes[user2]: ##Se o user2 tambem assistiu os mesmos filmes
+            si [item] = 1
+    if len(si) == 0:
+        return 0
+
+    ##Faz o somatório apenas para os filmes em comum entre os usuários
+    soma = sum([pow(avaliacoes[user1][item] - avaliacoes[user2][item],2)
+                for item in avaliacoes[user1] if item in avaliacoes[user2]])
+
+    return 1/(1+sqrt(soma))
+
+##Função que retorna a similaridade entre usuário
+
+
+
+
+
+
+
+
+
